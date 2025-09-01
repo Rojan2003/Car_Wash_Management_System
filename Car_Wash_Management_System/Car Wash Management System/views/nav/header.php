649e5a3558b521f6f747aef234f4aa6775637054
Rojan Derosa <rojanderosa101@gmail.com>
2025-09-01 20:48:33 +0800

    Add navbar styling and carousel UI components
    - Style navbar with custom colors, hover effects, and toggler design
    - Implement carousel component for smooth image/content sliding

---
+++ Car_Wash_Management_System/Car Wash Management System/views/nav/header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Car Wash Management System</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<!--
+############################################## 
+################## CAROUSEL ##################
+############################################## 
+-->
+<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
+<link href="../assets/dist/css/carousel.css" rel="stylesheet">
+    
+    <!-- Bootstrap core CSS -->
+<link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<style>
+
+  .carousel{
+  width: 95%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  }
+
+
+    .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+  }
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+
+  .custom-navbar {
+  background-color: #1a1a2e;
+  }
+
+.custom-navbar .navbar-brand,
+.custom-navbar .nav-link {
+  color: #fff;
+  font-weight: 500;
+  transition: color 0.3s ease;
+  }
+
+.custom-navbar .nav-link:hover,
+.custom-navbar .nav-link.active {
+  color: #00e5ff;
+}
+
+.navbar-toggler {
+  border: 1px solid #00bcd4;
+}
+
+.navbar-toggler-icon {
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='aqua' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+}
+</style>
+</head>
+<body>
+<div class="m-4">
+    <nav class="navbar navbar-expand-lg custom-navbar fixed-top">
+      <div class="container-fluid px-4">
+        <a class="navbar-brand" href="#">CarWashPro</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="navbar-nav">
+            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">Services</a>
+            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="#">About</a>
+          </div>
+          <div class="navbar-nav ms-auto">
+            <a class="nav-link" href="#">Login</a>
+          </div>
+        </div>
+      </div>
+</nav>
+</div>
